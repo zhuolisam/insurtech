93d11250aec9515741bd9b508033abd332d370db
zhuolisam <zhuolisam0627@gmail.com>
2022-09-10 18:48:34 +0800

    init

---
+++ src/partials/NavBar.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useRouter } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import SearchModal from "./header/SearchModal";
+import Notifications from "./header/Notifications";
+import Help from "./header/Help";
+import UserMenu from "./header/UserMenu";
+
+function NavBar({ sidebarOpen, setSidebarOpen }) {
+  const { pathname } = useLocation();
+
+  return (
+    <div className="w-full mb-3 text-lg text-slate-500 tracking-wide">
+      <ul className="flex flex-row flex-wrap border-b-[1px] border-slate-300 list-none">
+      <li
+          className={`px-4 py-2  duration-300 ease-out -mb-[1px] hover:text-slate-700
+          ${pathname === "/performance" && "border-b-[2px] border-b-indigo-500 text-slate-700"}`}
+        >
+          <NavLink end to="/performance">
+            <span className="nav-link cursor-pointer font-semibold hover:text-slate-700 duration-300 ease-out">Performance</span>
+          </NavLink>
+        </li>
+        <li
+          className={`px-4 py-2  duration-300 ease-out -mb-[1px] hover:text-slate-700
+          ${pathname === "/" && "border-b-[2px] border-b-indigo-500 text-slate-700"}`}
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+        >
+          <NavLink end to="/">
+            <span className="nav-link cursor-pointer active font-semibold">Traffic</span>
+          </NavLink>
+        </li>
+
+        <li
+          className={`px-4 py-2  duration-300 ease-out -mb-[1px] hover:text-slate-700
+          ${pathname === "/alerts" && "border-b-[2px] border-b-indigo-500 text-slate-700"}`}
+        >
+          <NavLink end to="/referral">
+            <span className="nav-link cursor-pointer font-semibold hover:text-slate-700 duration-300 ease-out">Referrals</span>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/partials/dashboard/1.SEOPositionTracking.jsx
@@ -0,0 +1,138 @@
+import React from 'react';
+
+function SEOPositionTracking() {
+
+
+
+
+  return (
+    <div className="col-span-full xl:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
+      <header className="px-5 py-4 border-b border-slate-100">
+        <h2 className="font-semibold text-slate-800">Top Keywords</h2>
+      </header>
+      <div className="p-3">
+
+        {/* Table */}
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full">
+            {/* Table header */}
+            <thead className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm">
+              <tr>
+                <th className="p-2">
+                  <div className="font-semibold text-left">Keyword</div>
+                </th>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Position</div>
+                </th>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Traffic</div>
+                </th>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Volume</div>
+                </th>
+                <th className="p-2">
+                  <div className="font-semibold text-center">Visibility</div>
+                </th>
+              </tr>
+            </thead>
+            {/* Table body */}
+            <tbody className="text-sm font-medium divide-y divide-slate-100">
+              {/* Row */}
+              <tr>
+                <td className="p-2">
+                  <div className="flex items-center">
+                    <div className="text-slate-800">fwd</div>
+                  </div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center">1  
+                  <span className='text-slate-400'>  0</span>
+                  </div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-green-500">20.22%</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center">18,100</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-sky-500">6.52%</div>
+                </td>
+              </tr>
+              {/* Row */}
+              <tr>
+                <td className="p-2">
+                  <div className="flex items-center">
+                    <div className="text-slate-800">omicron病徵</div>
+                  </div>
+                </td>
+                <td className="p-2">
+                <div className="text-center">2  
+                  <span className='text-red-500'>  &#8595;1</span>
+                  </div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-green-500">5.31%</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center">8,100</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-sky-500">2.61%</div>
+                </td>
+              </tr>
+              {/* Row */}
+              <tr>
+                <td className="p-2">
+                  <div className="flex items-center">
+
+                    <div className="text-slate-800">omicron 病徵</div>
+                  </div>
+                </td>
+                <td className="p-2">
+                <div className="text-center">3  
+                  <span className='text-red-500'>  &#8595;1</span>
+                  </div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-green-500">5.09%</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center">40,500</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-sky-500">5.90%</div>
+                </td>
+              </tr>
+              {/* Row */}
+              <tr>
+                <td className="p-2">
+                  <div className="flex items-center">
+                    <div className="text-slate-800">保險</div>
+                  </div>
+                </td>
+                <td className="p-2">
+                <div className="text-center">10  
+                  <span className='text-green-500'>  &#8593;1</span>
+                  </div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-green-500">4.91%</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center">4,400</div>
+                </td>
+                <td className="p-2">
+                  <div className="text-center text-sky-500">1.51%</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SEOPositionTracking;
